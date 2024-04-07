0c38252bd1376e3b95ee6c0a34f9591e0caa79de
FranciscoRodBel <9442@cifpceuta.es>
2024-04-07 20:54:08 +0200

    hovers y transiciones añadidas al nav

--- FrancisGol/view/templates/nav.php
+++ FrancisGol/view/templates/nav.php
@@ -4,6 +4,6 @@
     <a href="http://"><span>Competiciones</a></span>
     <a href="http://"><span>Equipos</span></a>
     <a href="http://"><span>Plantillas</span></a>
-    <a href="http://" class="vista_ordenador">Iniciar sesion</a>
+    <a href="http://" class="vista_ordenador">Iniciar sesión</a>
     <a href="http://" class="vista_ordenador">Registrarse</a>
 </nav>
