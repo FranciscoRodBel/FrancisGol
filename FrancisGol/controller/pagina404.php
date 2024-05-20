14a0816f1ed8af18c33469c66407cfb1cca8c9a8
franciscorodbel <9442.@cifpceuta.com>
2024-05-20 14:34:57 +0200

    página para errores añadida

---
+++ FrancisGol/controller/pagina404.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "../model/Usuario.php";
+    
+    $titulo = "FrancisGol - Página no encontrada";
+    $lista_css = [];
+
+    include '../view/templates/head.php';
+    include '../view/templates/header.php';
+    include '../view/templates/nav.php';
+    include '../view/pagina404.php';
+    include '../view/templates/footer.php';
---
+++ FrancisGol/view/pagina404.php
@@ -0,0 +1,8 @@
+<main>
+    <h1 class="titulo_pagina">Página no encontrada</h1>
+    <article>
+        <section class="pagina_no_encontrada">
+               <img src="../view/assets/images/pagina404.png" alt="página 404">
+        </section>
+    </article>
+</main>
